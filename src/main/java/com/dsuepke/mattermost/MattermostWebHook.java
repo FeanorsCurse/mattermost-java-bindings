ed63c64a68b6b8c20b2a5e25538da4d83776e25a
Daniel Süpke <daniel.suepke@wesustain.com>
2018-03-23 08:52:10 +0100

    CODE: Added NonNull and Nullable annotations CODE: Worked on code inspections cleanup

--- src/main/java/com/dsuepke/mattermost/MattermostWebHook.java
+++ src/main/java/com/dsuepke/mattermost/MattermostWebHook.java
@@ -2,32 +2,36 @@ package com.dsuepke.mattermost;
 
 import java.io.IOException;
 
+import javax.annotation.Nullable;
+
 import org.json.JSONObject;
 
+import lombok.NonNull;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * @author dsuepke
  */
+@SuppressWarnings("WeakerAccess") // Only relevant in this small example
 public class MattermostWebHook {
 
 	private static final MediaType JSON = MediaType.parse("application/json; encoding=UTF-8");
 
-	private OkHttpClient client = new OkHttpClient();
+	private final OkHttpClient client = new OkHttpClient();
 
-	private String webHookURL;
+	private final String webHookURL;
 
 
-	public MattermostWebHook(String webHookURL) {
+	public MattermostWebHook(@NonNull String webHookURL) {
 		this.webHookURL = webHookURL;
 	}
 
 
-	public void sendMessage(String text, String userName) throws IOException {
+	public void sendMessage(@Nullable String text, @Nullable String userName) throws IOException {
 		JSONObject json = new JSONObject();
 		json.put("text", text);
 		json.put("username", userName);
@@ -36,13 +40,20 @@ public class MattermostWebHook {
 	}
 
 
-	private String post(String json) throws IOException {
+	@SuppressWarnings("UnusedReturnValue")
+	@NonNull
+	private String post(@NonNull String json) throws IOException {
 		RequestBody body = RequestBody.create(JSON, json);
 		Request request = new Request.Builder()
 				.url(webHookURL)
 				.post(body)
 				.build();
-		Response response = client.newCall(request).execute();
-		return response.body().string();
+		ResponseBody response = client.newCall(request).execute().body();
+
+		if (response == null) {
+			throw new IOException("No response received from " + webHookURL);
+		}
+
+		return response.string();
 	}
 }
